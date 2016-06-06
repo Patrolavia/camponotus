cc575f7c38ebe5cca74de00b0bb56f222f18a75b
Ronmi Ren <ronmi.ren@gmail.com>
2016-06-06 18:02:30 +0800

    enable keyboard markup

--- example/mp.go
+++ example/mp.go
@@ -17,6 +17,27 @@ func (p *messageProcessor) Run() {
 	for m := range p.CH {
 		msg := fmt.Sprintf("Received message:\n```\n%#s\n```\nEntities: %v", toString(m), m.EntityText())
 		if _, err := p.API.SendMessage(m.From.Identifier(), msg, &telegram.Options{
+			ReplyMarkup: &telegram.ReplyKeyboardMarkup{
+				Keyboard: [][]telegram.KeyboardButton{
+					[]telegram.KeyboardButton{
+						telegram.KeyboardButton{
+							Text: "one",
+						},
+						telegram.KeyboardButton{
+							Text: "two",
+						},
+					},
+					[]telegram.KeyboardButton{
+						telegram.KeyboardButton{
+							Text: "three",
+						},
+						telegram.KeyboardButton{
+							Text: "four",
+						},
+					},
+				},
+				Once: true,
+			},
 			ParseMode: telegram.MarkdownMode,
 		}); err != nil {
 			log.Fatalf("messageProcessor: failed to send message: %s", err)
